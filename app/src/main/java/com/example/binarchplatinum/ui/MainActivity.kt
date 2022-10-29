55c1d851616b69b6508301d95efb521096fddd22
yordawira <50675414+yordawira@users.noreply.github.com>
2022-10-29 15:29:54 +0700

    feature/feature-loginpage add layout login and logic login yorda

--- app/src/main/java/com/example/binarchplatinum/ui/MainActivity.kt
+++ app/src/main/java/com/example/binarchplatinum/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.binarchplatinum
+package com.example.binarchplatinum.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.binarchplatinum.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
