d8bcb494f815355b145d6ca6203a2e8dc6b76426
yordawira <50675414+yordawira@users.noreply.github.com>
2022-11-04 13:24:04 +0700

    feature/feature revisi layout and color yorda

--- app/src/main/java/com/example/binarchplatinum/MainActivity.kt
+++ app/src/main/java/com/example/binarchplatinum/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.binarchplatinum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.binarchplatinum.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding:ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    companion object{
+        const val EXTRA_NAME: String = "extra_name"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        setTitleName()
+    }
+
+    private fun setTitleName () {
+        val name = intent.getStringExtra(EXTRA_NAME)
+        binding.tvName.text = buildString {
+            append("")
+            append(name)
+        }
     }
 }
