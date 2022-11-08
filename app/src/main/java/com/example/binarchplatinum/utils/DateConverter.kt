6dc4a869f1e41f8457362c5c7382796fb36f3f6b
dhewa.putra <dhewa>
2022-11-08 16:29:08 +0700

    Added more for DateConverter

--- app/src/main/java/com/example/binarchplatinum/utils/DateConverter.kt
+++ app/src/main/java/com/example/binarchplatinum/utils/DateConverter.kt
@@ -1,6 +1,7 @@
 package com.example.binarchplatinum.utils
 
 import androidx.room.TypeConverter
+import java.math.BigDecimal
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -22,5 +23,27 @@ object DateConverters {
     @TypeConverter
     fun dateToTimestamp(date: Date): Long {
         return date.time
+    }
+
+    @TypeConverter
+    fun bigDecimalToString(input: BigDecimal?): String {
+        return input?.toPlainString() ?: ""
+    }
+
+    @TypeConverter
+    fun bigDecimalToDouble(input: BigDecimal?): Double {
+        return input?.toDouble() ?: 0.0
+    }
+
+    @TypeConverter
+    fun stringToBigDecimal(input: String?): BigDecimal {
+        if (input.isNullOrBlank()) return BigDecimal.valueOf(0.0)
+        return input.toBigDecimalOrNull() ?: BigDecimal.valueOf(0.0)
+    }
+
+    @TypeConverter
+    fun doubleToBigDecimal(input: Double?): BigDecimal {
+        if (input == null) return BigDecimal.ZERO
+        return BigDecimal.valueOf(input) ?: BigDecimal.ZERO
     }
 }
